a700ffa67064cf0d95dca092a351f5efb3f56efc
Aaron Lim <aaron.lim.yu.kwang@gmail.com>
2018-02-08 21:23:19 +0800

    ok

---
+++ src/features/connectedForm/ConnectedAddForm/ConnectedAddForm.js
@@ -0,0 +1,35 @@
+// @flow
+import React from 'react'
+import { connect } from 'react-redux'
+import * as action from '../action'
+import ConnectedForm from '../ConnectedForm'
+
+
+const ConnectedEditFormComponent = (props) => {
+  if (!props.hasData) return null
+  
+  return (
+    <ConnectedForm {...props} onSubmit={props.saveAdd}>
+      {props.children}
+    </ConnectedForm>
+  )
+}
+
+
+const mapStateToProps = (state, props) => {
+  const hasData = !!state.getIn(['connectedForm', 'fetchedData', props.url, 'data'])
+
+  return {
+    hasData,
+  }
+}
+
+const mapDispatchToProps = (dispatch, props) => {
+  return {
+    saveAdd(data) {
+      dispatch(action.saveAdd(props.url, data.toJS(), props.onSuccess))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ConnectedEditFormComponent)
--- src/features/connectedForm/index.js
+++ src/features/connectedForm/index.js
@@ -18,6 +18,7 @@ export default {
     core.addEpic(epic)
     core.setComponent('ConnectedForm', ConnectedForm, "connectedForm")
     core.setComponent('ConnectedEditForm', ConnectedEditForm, "connectedForm")
+    core.setComponent('ConnectedAddForm', ConnectedEditForm, "connectedAddForm")
     core.setFn('editForm', doEditForm)
     next()
   }
