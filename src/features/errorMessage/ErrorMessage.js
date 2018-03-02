b3165ac3558152ddbbddd7bedbc40078bb151f1d
Ivan Yeoh <ivanbone@gmail.com>
2018-03-02 13:56:08 +0800

    ok

--- src/features/errorMessage/ErrorMessage.js
+++ src/features/errorMessage/ErrorMessage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import * as actions from './actions';
+import * as actions from './action';
 
 const Container = styled.div`
   position: fixed;
